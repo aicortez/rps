71ebfe05d7e8be65e58709ece7c6c021a9d9ced6
Alejandro Cortez <dev@sk1ll.cl>
2024-02-12 19:25:52 -0300

    Added playGame function, which repeats the game 5 times. Also added scores for each game and final result

--- script.js
+++ script.js
@@ -15,26 +15,59 @@ function getComputerChoice() {
 
 const computerSelection = getComputerChoice();
 
+const tie = "It's a tie!"
+const win = "You win!"
+const lose = "You lose!"
+let playerScore = 0
+let computerScore = 0
+
 function playRound() {
     playerSelection = prompt(`Choose between ${rps[0]}, ${rps[1]} or ${rps[2]}`);
     playerSelection = (playerSelection.charAt(0).toUpperCase() + (playerSelection.slice(1).toLowerCase()));
+    const  computerSelection = getComputerChoice();
     if (playerSelection === computerSelection) {
         alert(`It's a tie! You both picked ${playerSelection}`)
+        return result = tie
     }
     else if (playerSelection === "Rock" && computerSelection === "Scissors") {
         alert(`You win, ${playerSelection} beats ${computerSelection}`)
+        playerScore = playerScore+1
+        return result = win
     }
     else if (playerSelection === "Papers" && computerSelection === "Rock") {
         alert(`You win, ${playerSelection} beats ${computerSelection}`)
+        playerScore = playerScore+1
+        return result = win
     }
     else if (playerSelection === "Scissors" && computerSelection === "Papers") {
         alert(`You win, ${playerSelection} beats ${computerSelection}`)
+        playerScore = playerScore+1
+        return result = win
     } else {
         alert(`You lose, ${computerSelection} beats ${playerSelection}`)
+        computerScore = computerScore+1
+        return result = lose
     }
 }
 
 function playGame() {
+    playRound(); // game 1
+    console.log(result);
+    playRound(); // game 2
+    console.log(result);
+    playRound(); // game 3
+    console.log(result);
+    playRound(); // game 4
+    console.log(result);
+    playRound(); // game 5
+    console.log(result);
+    if (playerScore === computerScore) {
+        alert(`${tie} Score: ${playerScore} - ${computerScore}`)
+    } else if (playerScore > computerScore) {
+        alert(`${win} Score: ${playerScore} - ${computerScore}`)
+    } else {
+        alert(`${lose} Score: ${playerScore} - ${computerScore}`)
+    }
 }
 
-playRound();
+playGame();
