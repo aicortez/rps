ad4ed518183292f932790ebd65628d003767d684
Alejandro Cortez <dev@sk1ll.cl>
2024-02-12 18:15:30 -0300

    Fixed logic for the playRound function

--- script.js
+++ script.js
@@ -13,29 +13,28 @@ function getComputerChoice() {
     return rps[Math.floor(Math.random()*3)];
 }
 
-const playerSelection = "Rock";
 const computerSelection = getComputerChoice();
 
-
-function playRound(playerSelection, computerSelection) {
+function playRound() {
+    playerSelection = prompt(`Choose between ${rps[0]}, ${rps[1]} or ${rps[2]}`);
+    playerSelection = (playerSelection.charAt(0).toUpperCase() + (playerSelection.slice(1).toLowerCase()));
     if (playerSelection === computerSelection) {
-        console.log(`It's a tie! You both picked ${playerSelection}`)
+        alert(`It's a tie! You both picked ${playerSelection}`)
+    }
+    else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        alert(`You win, ${playerSelection} beats ${computerSelection}`)
+    }
+    else if (playerSelection === "Papers" && computerSelection === "Rock") {
+        alert(`You win, ${playerSelection} beats ${computerSelection}`)
+    }
+    else if (playerSelection === "Scissors" && computerSelection === "Papers") {
+        alert(`You win, ${playerSelection} beats ${computerSelection}`)
     } else {
-        if (playerSelection === "Rock" && computerSelection === "Papers") {
-        console.log(`You win, ${playerSelection} beats ${computerSelection}`)
-        }
-        if (playerSelection === "Papers" && computerSelection === "Rock") {
-            console.log(`You win, ${playerSelection} beats ${computerSelection}`)
-        }
-        if (playerSelection === "Scissors" && computerSelection === "Papers") {
-            console.log(`You win, ${playerSelection} beats ${computerSelection}`)
-        } 
-        else {
-            console.log(`You lose, ${computerSelection} beats ${playerSelection}`)
-        }
+        alert(`You lose, ${computerSelection} beats ${playerSelection}`)
     }
 }
 
 function playGame() {
-
 }
+
+playRound();
