46c346caef2016434ff565b49e80113c39fb41bf
Alejandro Cortez <dev@sk1ll.cl>
2025-02-03 19:22:32 -0300

    Adds a basic UI

--- script.js
+++ script.js
@@ -1,65 +1,127 @@
-// https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
-//
-// PC will randomly choose Rock, Paper or Scissors at the start of the game
-// Player will input Rock, Paper or Scissors
-// Winner is determined after 5 rounds
+const rps = ["Rock", "Paper", "Scissors"]
+const rockButton = document.querySelector(".rock")
+const paperButton = document.querySelector(".paper")
+const scissorsButton = document.querySelector(".scissors")
+const span1 = document.createElement("span")
+const span2 = document.createElement("span")
+const span3 = document.createElement("span")
+const span4 = document.createElement("span")
+const h2 = document.createElement("h2")
+const usrChoice = document.querySelector(".usr-choice")
+const rdmChoice = document.querySelector(".rdm-choice")
+const roundResult = document.querySelector(".round-result")
+const gameScore = document.querySelector(".game-score")
+const finalResult = document.querySelector("h2")
 
-const rps = ["Rock", "Paper", "Scissors"];
-
-function getComputerChoice() {
-    return rps[Math.floor(Math.random()*3)];
-}
-
-const computerSelection = getComputerChoice();
+let userSelection = 0
+let opponentSelection = 0
+let result = 0
 
 const tie = "It's a tie!"
 const win = "You win!"
 const lose = "You lose!"
-let playerScore = 0
-let computerScore = 0
+let userScore = 0
+let opponentScore = 0
+
+rockButton.addEventListener("click" , () => {
+    if (userScore < 5 && opponentScore < 5) {
+        opponentSelection = randomChoice()
+        userSelection = rps[0]
+        playRound(userSelection, opponentSelection)
+        span1.remove()
+        span1.textContent = userSelection
+        usrChoice.appendChild(span1)
+        span2.textContent = opponentSelection
+        rdmChoice.appendChild(span2)
+        span3.textContent = result
+        roundResult.appendChild(span3)
+        const scoreArray = [userScore, opponentScore]
+        ongoingScore = scoreArray.join(" - ")
+        span4.textContent = ongoingScore
+        gameScore.appendChild(span4)
+    }
+    if (userScore >= 5) {
+        h2.textContent = "Congratulations, you won!"
+        gameScore.appendChild(h2)
+    } else  if (opponentScore >= 5) {
+        h2.textContent = "You lost, better luck next time!"
+        gameScore.appendChild(h2)
+    }
+});
+
+paperButton.addEventListener("click" , () => {
+    if (userScore < 5 && opponentScore < 5) {
+        opponentSelection = randomChoice()
+        userSelection = rps[1]
+        playRound(userSelection, opponentSelection)
+        span1.remove()
+        span1.textContent = userSelection
+        usrChoice.appendChild(span1)
+        span2.textContent = opponentSelection
+        rdmChoice.appendChild(span2)
+        span3.textContent = result
+        roundResult.appendChild(span3)
+        const scoreArray = [userScore, opponentScore]
+        ongoingScore = scoreArray.join(" - ")
+        span4.textContent = ongoingScore
+        gameScore.appendChild(span4)
+    }
+    if (userScore >= 5) {
+        h2.textContent = "Congratulations, you won!"
+        gameScore.appendChild(h2)
+    } else  if (opponentScore >= 5) {
+        h2.textContent = "You lost, better luck next time!"
+        gameScore.appendChild(h2)
+    }
+});
+
+scissorsButton.addEventListener("click" , () => {  
+    if (userScore < 5 && opponentScore < 5) {
+        opponentSelection = randomChoice()
+        userSelection = rps[2]
+        playRound(userSelection, opponentSelection)
+        span1.remove()
+        span1.textContent = userSelection
+        usrChoice.appendChild(span1)
+        span2.textContent = opponentSelection
+        rdmChoice.appendChild(span2)
+        span3.textContent = result
+        roundResult.appendChild(span3)
+        const scoreArray = [userScore, opponentScore]
+        ongoingScore = scoreArray.join(" - ")
+        span4.textContent = ongoingScore
+        gameScore.appendChild(span4)
+    } else if (userScore > opponentScore) {
+        h2.textContent = "Congratulations, you won!"
+        gameScore.appendChild(h2)
+    } else {
+        h2.textContent = "You lost, better luck next time!"
+        gameScore.appendChild(h2)
+    }
+});
+
+function randomChoice() {
+    return rps[Math.floor(Math.random()*3)]
+}
 
 function playRound() {
-    playerSelection = prompt(`Choose between ${rps[0]}, ${rps[1]} or ${rps[2]}`);
-    playerSelection = (playerSelection.charAt(0).toUpperCase() + (playerSelection.slice(1).toLowerCase()));
-    const computerSelection = getComputerChoice(); // Randomize selection at the start of each round
-    if (playerSelection === computerSelection) {
-        alert(`It's a tie! You both picked ${playerSelection}`)
-        return result = tie
+    if (userSelection === opponentSelection) {
+        result = tie
     }
-    else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        alert(`You win, ${playerSelection} beats ${computerSelection}`)
-        playerScore = playerScore+1
-        return result = win
+    else if (userSelection === "Rock" && opponentSelection === "Scissors") {
+        userScore++
+        result = win
     }
-    else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        alert(`You win, ${playerSelection} beats ${computerSelection}`)
-        playerScore = playerScore+1
-        return result = win
+    else if (userSelection === "Paper" && opponentSelection === "Rock") {
+        userScore++
+        result = win
     }
-    else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        alert(`You win, ${playerSelection} beats ${computerSelection}`)
-        playerScore = playerScore+1
-        return result = win
+    else if (userSelection === "Scissors" && opponentSelection === "Paper") {
+        userScore++
+        result = win
     } else {
-        alert(`You lose, ${computerSelection} beats ${playerSelection}`)
-        computerScore = computerScore+1
-        return result = lose
+        opponentScore++
+        result = lose
     }
+    return result;
 }
-
-function playGame() {
-    playRound(); // game 1
-    playRound(); // game 2
-    playRound(); // game 3
-    playRound(); // game 4
-    playRound(); // game 5
-    if (playerScore === computerScore) {
-        alert(`${tie} Score: ${playerScore} - ${computerScore}`)
-    } else if (playerScore > computerScore) {
-        alert(`${win} Score: ${playerScore} - ${computerScore}`)
-    } else {
-        alert(`${lose} Score: ${playerScore} - ${computerScore}`)
-    }
-}
-
-playGame(); // Initializes the game
